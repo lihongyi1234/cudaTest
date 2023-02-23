774986d8fc18bd1966f502567475669d3b8ee191
lihongyi1234 <36233282+lihongyi1234@users.noreply.github.com>
2023-02-23 10:26:51 +0800

    Add files via upload

--- test_cuda.cpp
+++ test_cuda.cpp
@@ -5,20 +5,7 @@ int main(int argc, char**argv)
 {
 	std::string jpg_fn = argv[1];
 	cv::Mat image = cv::imread(jpg_fn, CV_8U);
-	int count = 0;
-	for (int row = 0; row < image.rows; row++) {
-		for (int col = 0; col < image.cols; col++) {
-			uchar value = image.at<uchar>(row, col);
-			if (value > 0) {
-				count++;
-			}
-
-		}
-	}
-	std::cout << "count: " << count << std::endl;
-
-	int new_count = getImageNonzeroCount(image);
-
-	std::cout << "new_count: " << new_count<< std::endl;
+	int count = getImageNonzeroCount(image);
+	std::cout << "count: " << count<< std::endl;
 	return 0;
 }
